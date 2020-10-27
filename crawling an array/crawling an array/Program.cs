ce47cf7cd79cf3e18997d4e76bd194a4190d4e93
iliyankolev93 <69952887+iliyankolev93@users.noreply.github.com>
2020-10-27 19:41:41 +0200

    twisted array

---
+++ crawling an array/crawling an array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace crawling_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] myNumbers =
+            {
+                2.5, -10000, 5, 76, 3, -2, 0, 13, -651, 33.33, 1
+            };
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                Console.WriteLine(myNumbers[i]);
+            }
+        }
+    }
+}
